47c8103f6d966e4bf22ca74bd35a59a494fc62ab
Patrick Prémartin <patrick.premartin@developpeur-pascal.fr>
2020-12-04 18:55:34 +0100

    VCL template project with user interface ready

---
+++ ProjectTemplate/Unit1.pas
@@ -0,0 +1,38 @@
+unit Unit1;
+
+// My solution for https://adventofcode.com/2020/day/4
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    Panel1: TPanel;
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Memo1.Lines.Clear;
+  Edit1.Text := '';
+end;
+
+end.
