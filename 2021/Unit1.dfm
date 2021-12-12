1357c45528dee2de0c998c834b0ce8883fb3b76e
patrick@premartin.fr <Patrick>
2021-12-12 12:05:13 +0100

    Advent Of Code 2021 - Day 1

---
+++ 2021/Unit1.dfm
@@ -0,0 +1,30 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 15
+  object Memo1: TMemo
+    Left = 0
+    Top = 0
+    Width = 624
+    Height = 441
+    Align = alClient
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 0
+    ExplicitLeft = 232
+    ExplicitTop = 200
+    ExplicitWidth = 185
+    ExplicitHeight = 89
+  end
+end
---
+++ 2021/Unit1.pas
@@ -0,0 +1,190 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    procedure FormShow(Sender: TObject);
+  private
+    { Déclarations privées }
+    FichierOuvert: string;
+    Fichier: Text;
+    function getLigne(NomFichier: string): string;
+    function FinDeFichier: boolean;
+    procedure FermeFichier;
+    procedure OuvreFichier(NomFichier: string);
+    procedure AfficheResultat(Jour, Exercice: byte; Reponse: int64);
+  public
+    { Déclarations publiques }
+    procedure Jour01_1;
+    procedure Jour01_2;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.AfficheResultat(Jour, Exercice: byte; Reponse: int64);
+begin
+  Memo1.Lines.Add('Jour ' + Jour.ToString + ' Exercice ' + Exercice.ToString +
+    ' Reponse ' + Reponse.ToString);
+end;
+
+procedure TForm1.FermeFichier;
+begin
+  if not FichierOuvert.IsEmpty then
+  begin
+    closefile(Fichier);
+    FichierOuvert := '';
+  end;
+end;
+
+function TForm1.FinDeFichier: boolean;
+begin
+  result := (FichierOuvert = '') or eof(Fichier);
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Memo1.Lines.Clear;
+  Jour01_1;
+  Jour01_2;
+end;
+
+function TForm1.getLigne(NomFichier: string): string;
+begin
+  if (NomFichier <> FichierOuvert) then
+    OuvreFichier(NomFichier);
+  if (not FinDeFichier) then
+    readln(Fichier, result);
+end;
+
+procedure TForm1.Jour01_1;
+var
+  PremiereLigneTraitee: boolean;
+  Valeur_Ligne: word;
+  Ligne: string;
+  Valeur_Precedente: word;
+  Nb_Increments: int64;
+begin
+  PremiereLigneTraitee := false;
+  Valeur_Precedente := 0;
+  Nb_Increments := 0;
+  repeat
+    Ligne := getLigne('..\..\input-01.txt');
+    try
+      Valeur_Ligne := Ligne.ToInteger;
+      if PremiereLigneTraitee then
+      begin
+        if Valeur_Ligne > Valeur_Precedente then
+          inc(Nb_Increments);
+        Valeur_Precedente := Valeur_Ligne;
+      end
+      else
+      begin
+        Valeur_Precedente := Valeur_Ligne;
+        PremiereLigneTraitee := true;
+      end;
+    except
+
+    end;
+  until FinDeFichier;
+  FermeFichier;
+  AfficheResultat(1, 1, Nb_Increments);
+end;
+
+procedure TForm1.Jour01_2;
+var
+  TroisPremieresLignesTraitees: boolean;
+  Valeur_Ligne: word;
+  Ligne: string;
+  Valeur_1, Valeur_2: word;
+  Nb_Increments: int64;
+  Tab_Valeurs: array [0 .. 2] of word;
+  Num_Ligne: int64;
+begin
+  TroisPremieresLignesTraitees := false;
+  Valeur_1 := 0;
+  Valeur_2 := 0;
+  Nb_Increments := 0;
+  Num_Ligne := 0;
+  Tab_Valeurs[0] := 0;
+  Tab_Valeurs[1] := 0;
+  Tab_Valeurs[2] := 0;
+  repeat
+    Ligne := getLigne('..\..\input-01.txt');
+    try
+      Valeur_Ligne := Ligne.ToInteger;
+      // On fait le cumul avec les valeurs existantes avant comparaison
+      case Num_Ligne mod 3 of
+        0:
+          begin
+            inc(Tab_Valeurs[1], Valeur_Ligne);
+            inc(Tab_Valeurs[2], Valeur_Ligne);
+          end;
+        1:
+          begin
+            inc(Tab_Valeurs[0], Valeur_Ligne);
+            inc(Tab_Valeurs[2], Valeur_Ligne);
+          end;
+        2:
+          begin
+            inc(Tab_Valeurs[0], Valeur_Ligne);
+            inc(Tab_Valeurs[1], Valeur_Ligne);
+          end;
+      end;
+      // Comparaison des valeurs à partir de la quatrième ligne remplie
+      if TroisPremieresLignesTraitees then
+      begin
+        case Num_Ligne mod 3 of
+          0:
+            begin
+              Valeur_1 := Tab_Valeurs[0];
+              Valeur_2 := Tab_Valeurs[1];
+            end;
+          1:
+            begin
+              Valeur_1 := Tab_Valeurs[1];
+              Valeur_2 := Tab_Valeurs[2];
+            end;
+          2:
+            begin
+              Valeur_1 := Tab_Valeurs[2];
+              Valeur_2 := Tab_Valeurs[0];
+            end;
+        end;
+        if Valeur_2 > Valeur_1 then
+          inc(Nb_Increments);
+      end
+      else if (Num_Ligne = 2) then
+        TroisPremieresLignesTraitees := true;
+      // On initialise la valeur de la ligne en cours pour partir sur un nouveau cycle de 3 lignes
+      Tab_Valeurs[Num_Ligne mod 3] := Valeur_Ligne;
+      // Passage à la ligne suivante
+      inc(Num_Ligne);
+    except
+
+    end;
+  until FinDeFichier;
+  FermeFichier;
+  AfficheResultat(1, 1, Nb_Increments);
+end;
+
+procedure TForm1.OuvreFichier(NomFichier: string);
+begin
+  FermeFichier;
+  assignfile(Fichier, NomFichier);
+  reset(Fichier);
+  FichierOuvert := NomFichier;
+end;
+
+end.
