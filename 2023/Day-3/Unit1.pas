d1141b39fada0fc6e6eceaac6f806a997e5683b4
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-12-04 22:01:10 +0100

    added a new approch for Day 3 Ex 2

--- 2023/Day-3/Unit1.pas
+++ 2023/Day-3/Unit1.pas
@@ -8,8 +8,8 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.WinXCtrls, Vcl.StdCtrls;
 
 Const
-  // CDataFile = '..\..\input.txt';
-  CDataFile = '..\..\input-test.txt';
+  CDataFile = '..\..\input.txt';
+  // CDataFile = '..\..\input-test.txt';
 
 type
   TForm1 = class(TForm)
@@ -48,6 +48,8 @@ uses
   System.DateUtils,
   System.RegularExpressions,
   System.Diagnostics,
+  System.Generics.Collections,
+  System.Generics.Defaults,
   System.IOUtils;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -209,14 +211,14 @@ begin
       end
       else if NumberValid then
       begin
-        AddLog('Ok : ' + CurrentNumber.tostring);
+        // AddLog('Ok : ' + CurrentNumber.tostring);
         result := result + CurrentNumber;
         NumberValid := false;
         CurrentNumber := 0;
       end
       else if (CurrentNumber > 0) then
       begin
-        AddLog('Not Ok : ' + CurrentNumber.tostring);
+        // AddLog('Not Ok : ' + CurrentNumber.tostring);
         CurrentNumber := 0;
       end;
     end;
@@ -367,78 +369,175 @@ begin
       end;
 end;
 
+type
+  TEtoile = class
+  public
+    Value: cardinal;
+    AuMoinsDeux: boolean;
+    constructor Create;
+  end;
+
+  TEtoiles = TObjectDictionary<cardinal, TEtoile>;
+
 function TForm1.Exercice2Bis: cardinal;
 var
   Lignes: TArray<string>;
   i: integer;
   CurrentNumber: cardinal;
   j: integer;
-  Asteriskfound: boolean;
-  // TODO : à faire
+  etoile: TEtoile;
+  ListeEtoiles: TEtoiles;
+  prevcoord, Coord: cardinal;
+  ListeCoord: TList<cardinal>;
+  NbColParLigne: integer;
   // stocker liste de coordonnées des étoiles avec la valeur en cours et le fait d'avoir 2 nombres multipliés
   // lors du parcours, pour chaque chiffre on regarde si une étoile est présente (au lieu du test d'avant)
   // si c'est oui, on liste les coordonnées de la ou des étoiles concernées par ce nombre
   // en fin de parcours du nombre, on traite la multiplication liée aux étoiles trouvées pour lui
   // en fin de fichier on cumule le total des valeurs des étoiles ayant eu des nombres multipliés entre eux
 begin
-  AddLog('not finished');
-  exit;
-  Lignes := tfile.ReadAllLines(CDataFile);
-  result := 0;
-  for i := 0 to length(Lignes) - 1 do
-  begin
-    Asteriskfound := false;
-    CurrentNumber := 0;
-    for j := 0 to length(Lignes[i]) - 1 do
-    begin
-      if Lignes[i].Chars[j] in ['0' .. '9'] then
+  ListeEtoiles := TEtoiles.Create([doOwnsValues]);
+  try
+    ListeCoord := TList<cardinal>.Create;
+    try
+      Lignes := tfile.ReadAllLines(CDataFile);
+      result := 0;
+      NbColParLigne := length(Lignes[0]);
+      // on suppose que la ligne n'est pas vide et
+      // que le nombre de caractères est le même sur chaque
+      for i := 0 to length(Lignes) - 1 do
       begin
-        CurrentNumber := CurrentNumber * 10 + strtoint(Lignes[i].Chars[j]);
-        if not Asteriskfound then
+        CurrentNumber := 0;
+        ListeCoord.Clear;
+        for j := 0 to length(Lignes[i]) - 1 do
         begin
-          // à gauche du chiffre
-          Asteriskfound := (j > 0) and (Lignes[i].Chars[j - 1] = '*');
-          // en haut à gauche du chiffre
-          Asteriskfound := Asteriskfound or
-            ((j > 0) and (i > 0) and (Lignes[i - 1].Chars[j - 1] = '*'));
-          // en bas à gauche du chiffre
-          Asteriskfound := Asteriskfound or
-            ((j > 0) and (i < length(Lignes) - 1) and
-            (Lignes[i + 1].Chars[j - 1] = '*'));
-          // à droite du chiffre
-          Asteriskfound := Asteriskfound or
-            ((j < length(Lignes[i]) - 1) and (Lignes[i].Chars[j + 1] = '*'));
-          // en haut à droite du chiffre
-          Asteriskfound := Asteriskfound or
-            ((i > 0) and (j < length(Lignes[i - 1]) - 1) and
-            (Lignes[i - 1].Chars[j + 1] = '*'));
-          // en bas à droite du chiffre
-          Asteriskfound := Asteriskfound or
-            ((i < length(Lignes) - 1) and (j < length(Lignes[i + 1]) - 1) and
-            (Lignes[i + 1].Chars[j + 1] = '*'));
-          // au dessus du chiffre
-          Asteriskfound := Asteriskfound or
-            ((i > 0) and (Lignes[i - 1].Chars[j] = '*'));
-          // sous le chiffre
-          Asteriskfound := Asteriskfound or
-            ((i < length(Lignes) - 1) and (Lignes[i + 1].Chars[j] = '*'));
+          if Lignes[i].IsEmpty then
+            continue;
+
+          if Lignes[i].Chars[j] in ['0' .. '9'] then
+          begin
+            CurrentNumber := CurrentNumber * 10 + strtoint(Lignes[i].Chars[j]);
+
+            // à gauche du chiffre
+            if (j > 0) and (Lignes[i].Chars[j - 1] = '*') then
+              ListeCoord.Add(i * NbColParLigne + j - 1);
+
+            // en haut à gauche du chiffre
+            if ((j > 0) and (i > 0) and (Lignes[i - 1].Chars[j - 1] = '*')) then
+              ListeCoord.Add((i - 1) * NbColParLigne + j - 1);
+
+            // en bas à gauche du chiffre
+            if ((j > 0) and (i < length(Lignes) - 1) and
+              (Lignes[i + 1].Chars[j - 1] = '*')) then
+              ListeCoord.Add((i + 1) * NbColParLigne + j - 1);
+
+            // à droite du chiffre
+            if ((j < length(Lignes[i]) - 1) and (Lignes[i].Chars[j + 1] = '*'))
+            then
+              ListeCoord.Add(i * NbColParLigne + j + 1);
+
+            // en haut à droite du chiffre
+            if ((i > 0) and (j < length(Lignes[i - 1]) - 1) and
+              (Lignes[i - 1].Chars[j + 1] = '*')) then
+              ListeCoord.Add((i - 1) * NbColParLigne + j + 1);
+
+            // en bas à droite du chiffre
+            if ((i < length(Lignes) - 1) and (j < length(Lignes[i + 1]) - 1) and
+              (Lignes[i + 1].Chars[j + 1] = '*')) then
+              ListeCoord.Add((i + 1) * NbColParLigne + j + 1);
+
+            // au dessus du chiffre
+            if ((i > 0) and (Lignes[i - 1].Chars[j] = '*')) then
+              ListeCoord.Add((i - 1) * NbColParLigne + j);
+
+            // sous le chiffre
+            if ((i < length(Lignes) - 1) and (Lignes[i + 1].Chars[j] = '*'))
+            then
+              ListeCoord.Add((i + 1) * NbColParLigne + j);
+          end
+          else if (ListeCoord.Count > 0) then
+          begin
+            // AddLog('Ok : ' + CurrentNumber.tostring);
+            prevcoord := high(cardinal);
+            ListeCoord.Sort(TComparer<cardinal>.Construct(
+              function(const a, b: cardinal): integer
+              begin
+                if a = b then
+                  result := 0
+                else if a < b then
+                  result := -1
+                else
+                  result := 1;
+              end));
+            for Coord in ListeCoord do
+              if (Coord <> prevcoord) then
+              begin
+                prevcoord := Coord;
+                if not ListeEtoiles.TryGetValue(Coord, etoile) then
+                begin
+                  etoile := TEtoile.Create;
+                  etoile.Value := CurrentNumber;
+                  ListeEtoiles.Add(Coord, etoile);
+                end
+                else
+                begin
+                  etoile.Value := etoile.Value * CurrentNumber;
+                  etoile.AuMoinsDeux := true;
+                end;
+              end;
+            CurrentNumber := 0;
+            ListeCoord.Clear;
+          end
+          else if (CurrentNumber > 0) then
+          begin
+            // AddLog('Not Ok : ' + CurrentNumber.tostring);
+            CurrentNumber := 0;
+          end;
         end;
-      end
-      else if Asteriskfound then
-      begin
-        AddLog('Ok : ' + CurrentNumber.tostring);
-        result := result + CurrentNumber;
-        Asteriskfound := false;
-        CurrentNumber := 0;
-      end
-      else if (CurrentNumber > 0) then
-      begin
-        AddLog('Not Ok : ' + CurrentNumber.tostring);
-        CurrentNumber := 0;
+        if (ListeCoord.Count > 0) then
+        begin
+          // AddLog('Ok : ' + CurrentNumber.tostring);
+          prevcoord := high(cardinal);
+          ListeCoord.Sort(TComparer<cardinal>.Construct(
+            function(const a, b: cardinal): integer
+            begin
+              if a = b then
+                result := 0
+              else if a < b then
+                result := -1
+              else
+                result := 1;
+            end));
+          for Coord in ListeCoord do
+            if (Coord <> prevcoord) then
+            begin
+              prevcoord := Coord;
+              if not ListeEtoiles.TryGetValue(Coord, etoile) then
+              begin
+                etoile := TEtoile.Create;
+                etoile.Value := CurrentNumber;
+                ListeEtoiles.Add(Coord, etoile);
+              end
+              else
+              begin
+                etoile.Value := etoile.Value * CurrentNumber;
+                etoile.AuMoinsDeux := true;
+              end;
+            end;
+          CurrentNumber := 0;
+          ListeCoord.Clear;
+        end;
       end;
+      // calcul final
+      result := 0;
+      for etoile in ListeEtoiles.Values do
+        if etoile.AuMoinsDeux then
+          result := result + etoile.Value;
+    finally
+      ListeCoord.Free;
     end;
-    if Asteriskfound then
-      result := result + CurrentNumber;
+  finally
+    ListeEtoiles.Free;
   end;
 end;
 
@@ -460,6 +559,14 @@ begin
   while length(S) < 3 do
     S := '0' + S;
   result := TimeToStr(dt) + ',' + S;
+end;
+
+{ TEtoile }
+
+constructor TEtoile.Create;
+begin
+  Value := 0;
+  AuMoinsDeux := false;
 end;
 
 end.
