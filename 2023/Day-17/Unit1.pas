d330f8505f25934c1ff4199947b0c12ae906a9ab
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-01-01 00:34:07 +0100

    Advent of Code 2023 Day 17 Ex 1

--- 2023/Day-17/Unit1.pas
+++ 2023/Day-17/Unit1.pas
@@ -1,5 +1,17 @@
 unit Unit1;
 
+// L'algorithme de pathfinding de base utilisé a départ ne règle les déplacements
+// que case par case sans tenir compte de la contrainte du nombre maximum de
+// déplacements dans un sens.
+//
+// @NineBerry ayant partagé sa solutions en C# lors de la session sur Twitch,
+// j'ai repris ce qu'il a fait pour la version finale de ce projet.
+// https://dotnetfiddle.net/9DN6g5
+//
+// Dans mon cas j'ai le chemin parcourru, mais ce n'est pas nécessaire pour
+// résoudre l'exercice 1 donc on part sur une file d'attente de cases parcourues
+// dans un sens et avec un nombre courant de pas pour l'atteindre.
+
 interface
 
 uses
@@ -17,16 +29,44 @@ uses
   System.Generics.Collections;
 
 Const
-//  CDataFile = '..\..\input.txt';
-   CDataFile = '..\..\input-test.txt';
+  CDataFile = '..\..\input.txt';
+  // CDataFile = '..\..\input-test.txt';
+  CNbPasMiniEx1 = 1; // minimum 1 case avant de regarder à gauche et à droite
+  CNbPasMaxiEx1 = 3; // maximum 3 cases de déplacement
 
 const
   CPoidsMax = maxint - 10;
   // 10 étant le poids maxi d'une case (HeatLoss de 1 à 9)
 
 type
+{$SCOPEDENUMS ON}
+  TDirection = (haut, droite, bas, gauche);
+
+  TElem = class
+  public
+    // Données de base
+    X, Y: int64;
+    HeatLoss: int64;
+    // Provenant du déplacement
+    Direction: TDirection;
+    NbPasCourants: int64;
+    constructor Create(AX, AY: int64; AHeatLoss: int64; ADirection: TDirection;
+      ANbPasCourants: int64);
+  end;
+
+  TMyPriorityQueue = class(TObjectList<TElem>)
+  public
+    procedure Push(Elem: TElem);
+    function Pop: TElem;
+  end;
+
+  TCellsVisited = class(TObjectList<TElem>)
+    function hasVisitedElem(AX, AY: int64; ADirection: TDirection;
+      ANbPasCourant: int64): boolean;
+  end;
+
   TCell = record
-    HeatLoss: byte;
+    HeatLoss: int64;
     PathFindingPoidsCellule: int64;
     PathFindingDejaPasse: boolean;
   end;
@@ -55,8 +95,8 @@ type
   public
     { Déclarations publiques }
     Map: TMap;
-    function TrouveLaFin(X, Y: integer; VX, VY: integer): int64;
-    procedure PathFindingSurMap(EndX, EndY: integer);
+    function TrouveLaFin(X, Y: int64; VX, VY: int64): int64;
+    procedure PathFindingSurMap(EndX, EndY: int64);
     procedure DrawMap;
   end;
 
@@ -152,7 +192,7 @@ end;
 
 procedure TForm1.DrawMap;
 var
-  Col, Lig: integer;
+  Col, Lig: int64;
   S: string;
 begin
   AddLog('Heat loss :');
@@ -207,9 +247,131 @@ begin
 end;
 
 function TForm1.Exercice1: int64;
+
+  procedure VaVersLaGauche(Elem: TElem; Queue: TMyPriorityQueue);
+  var
+    newx, newy: int64;
+    newdirection: TDirection;
+  begin
+    case Elem.Direction of
+      TDirection.haut:
+        begin
+          newdirection := TDirection.gauche;
+          newx := Elem.X - 1;
+          newy := Elem.Y;
+        end;
+      TDirection.droite:
+        begin
+          newdirection := TDirection.haut;
+          newx := Elem.X;
+          newy := Elem.Y - 1;
+        end;
+      TDirection.bas:
+        begin
+          newdirection := TDirection.droite;
+          newx := Elem.X + 1;
+          newy := Elem.Y;
+        end;
+      TDirection.gauche:
+        begin
+          newdirection := TDirection.bas;
+          newx := Elem.X;
+          newy := Elem.Y + 1;
+        end;
+    end;
+
+    // Dans la grille ?
+    if (newx < 0) or (newy < 0) or (newx >= length(Map)) or
+      (newy >= length(Map[0])) then
+      exit;
+
+    Queue.Push(TElem.Create(newx, newy, Elem.HeatLoss + Map[newx,
+      newy].HeatLoss, newdirection, 1));
+  end;
+  procedure VaVersLaDroite(Elem: TElem; Queue: TMyPriorityQueue);
+  var
+    newx, newy: int64;
+    newdirection: TDirection;
+  begin
+    case Elem.Direction of
+      TDirection.haut:
+        begin
+          newdirection := TDirection.droite;
+          newx := Elem.X + 1;
+          newy := Elem.Y;
+        end;
+      TDirection.droite:
+        begin
+          newdirection := TDirection.bas;
+          newx := Elem.X;
+          newy := Elem.Y + 1;
+        end;
+      TDirection.bas:
+        begin
+          newdirection := TDirection.gauche;
+          newx := Elem.X - 1;
+          newy := Elem.Y;
+        end;
+      TDirection.gauche:
+        begin
+          newdirection := TDirection.haut;
+          newx := Elem.X;
+          newy := Elem.Y - 1;
+        end;
+    end;
+
+    // Dans la grille ?
+    if (newx < 0) or (newy < 0) or (newx >= length(Map)) or
+      (newy >= length(Map[0])) then
+      exit;
+
+    Queue.Push(TElem.Create(newx, newy, Elem.HeatLoss + Map[newx,
+      newy].HeatLoss, newdirection, 1));
+  end;
+  procedure VaDevant(Elem: TElem; Queue: TMyPriorityQueue);
+  var
+    newx, newy: int64;
+  begin
+    case Elem.Direction of
+      TDirection.haut:
+        begin
+          newx := Elem.X;
+          newy := Elem.Y - 1;
+        end;
+      TDirection.droite:
+        begin
+          newx := Elem.X + 1;
+          newy := Elem.Y;
+        end;
+      TDirection.bas:
+        begin
+          newx := Elem.X;
+          newy := Elem.Y + 1;
+        end;
+      TDirection.gauche:
+        begin
+          newx := Elem.X - 1;
+          newy := Elem.Y;
+        end;
+    end;
+
+    // Dans la grille ?
+    if (newx < 0) or (newy < 0) or (newx >= length(Map)) or
+      (newy >= length(Map[0])) then
+      exit;
+
+    Queue.Push(TElem.Create(newx, newy, Elem.HeatLoss + Map[newx,
+      newy].HeatLoss, Elem.Direction, Elem.NbPasCourants + 1));
+  end;
+
 var
-  Col, Lig: integer;
+  Col, Lig: int64;
   Lignes: TArray<string>;
+  Visited: TCellsVisited;
+  Queue: TMyPriorityQueue;
+  X, Y, nbPas: int64;
+  EndX, EndY: int64;
+  Elem: TElem;
 begin
   Lignes := tfile.ReadAllLines(CDataFile);
 
@@ -225,18 +387,81 @@ begin
       Map[Col][Lig].HeatLoss := strtoint(Lignes[Lig].chars[Col]);
       Map[Col][Lig].PathFindingDejaPasse := false;
     end;
-  DrawMap;
+  // DrawMap;
 
-  Map[0][0].PathFindingDejaPasse := true;
+{$REGION 'version path finding non adaptée à cet exercice'}
+  if false then
+  begin
+    // Parcours avec pathfinding qui ne peut pas fonctionner dans notre cas à
+    // cause des contraintes de nb case maximum lors du déplacement
+    Map[0][0].PathFindingDejaPasse := true;
+    Result := TrouveLaFin(0, 0, 1, 0);
+    DrawMap;
+  end;
+{$ENDREGION}
+{$REGION 'solution inspirée de @NineBerry (sur Twitch) : https://dotnetfiddle.net/9DN6g5'}
+  // implémentation de path finding selon l'algorithme de Dijskra
+  // en tenant compte du nombre de pas possibles dans une direction avant de
+  // devoir en changer
+  Result := -1;
+  Visited := TCellsVisited.Create;
+  try
+    Queue := TMyPriorityQueue.Create;
+    try
+      // On empile les mouvements possibles depuis la case de départ
+      Queue.Push(TElem.Create(0, 0, 0, TDirection.haut, 0));
+      Queue.Push(TElem.Create(0, 0, 0, TDirection.droite, 0));
+      Queue.Push(TElem.Create(0, 0, 0, TDirection.bas, 0));
+      Queue.Push(TElem.Create(0, 0, 0, TDirection.gauche, 0));
 
-  Result := TrouveLaFin(0, 0, 1, 0);
+      EndX := length(Map) - 1;
+      EndY := length(Map[0]) - 1;
 
-  DrawMap;
+      while (Queue.Count > 0) do
+      begin
+        Elem := Queue.Pop;
+
+        // if (Elem.X = EndX) and (Elem.Y = EndY) and (elem.NbPasCourants>=CNbPasMiniEx1) and (elem.NbPasCourants<=CNbPasMaxiEx1) then
+        if (Elem.X = EndX) and (Elem.Y = EndY) then
+        begin
+          Result := Elem.HeatLoss;
+          Elem.free;
+          break;
+        end;
+
+        if Visited.hasVisitedElem(Elem.X, Elem.Y, Elem.Direction,
+          Elem.NbPasCourants) then
+        begin
+          Elem.free;
+          continue;
+        end;
+
+        Visited.Add(Elem);
+
+        if (Elem.NbPasCourants >= CNbPasMiniEx1) then
+        // on ne peut tourner qu'après X pas minimum
+        begin
+          VaVersLaGauche(Elem, Queue);
+          VaVersLaDroite(Elem, Queue);
+        end;
+
+        if Elem.NbPasCourants < CNbPasMaxiEx1 then
+          // on ne peut pas avancer plus de X pas dans la même direction
+          VaDevant(Elem, Queue);
+      end;
+    finally
+      Queue.free;
+    end;
+  finally
+    Visited.free;
+  end;
+
+{$ENDREGION}
 end;
 
 function TForm1.Exercice2: int64;
 var
-  Lig: integer;
+  Lig: int64;
   Lignes: TArray<string>;
 begin
   Lignes := tfile.ReadAllLines(CDataFile);
@@ -267,11 +492,11 @@ begin
   Result := TimeToStr(dt) + ',' + S;
 end;
 
-procedure TForm1.PathFindingSurMap(EndX, EndY: integer);
+procedure TForm1.PathFindingSurMap(EndX, EndY: int64);
 var
-  Col, Lig: integer;
+  Col, Lig: int64;
   Modif: boolean;
-  a, b, c, d, z: integer;
+  a, b, c, d, z: int64;
 begin
   // algorithme utilisé : Dijkstra
   // https://fr.wikipedia.org/wiki/Algorithme_de_Dijkstra
@@ -331,26 +556,26 @@ begin
   end;
 end;
 
-function TForm1.TrouveLaFin(X, Y: integer; VX, VY: integer): int64;
+function TForm1.TrouveLaFin(X, Y: int64; VX, VY: int64): int64;
 var
-  NbCases: byte;
-  EndX, EndY: integer;
-  PoidsDevant, PoidsCote1, PoidsCote2: integer;
-  NbCol, NbLig: integer;
-  VSwap: integer;
+  NbCases: int64;
+  EndX, EndY: int64;
+  PoidsDevant, PoidsCote1, PoidsCote2: int64;
+  NbCol, NbLig: int64;
+  VSwap: int64;
 begin
   Result := 0;
 
   NbCol := length(Map);
   NbLig := length(Map[0]);
-//  AddLog('Taille : ' + NbCol.tostring + '*' + NbLig.tostring);
+  // AddLog('Taille : ' + NbCol.tostring + '*' + NbLig.tostring);
 
   if (X < 0) or (Y < 0) or (X >= NbCol) or (Y >= NbLig) then
     exit;
 
   EndX := length(Map) - 1;
   EndY := length(Map[0]) - 1;
-//  AddLog('Arrivee : ' + EndX.tostring + ',' + EndY.tostring);
+  // AddLog('Arrivee : ' + EndX.tostring + ',' + EndY.tostring);
 
   PathFindingSurMap(EndX, EndY);
 
@@ -404,6 +629,56 @@ begin
     Map[X][Y].PathFindingDejaPasse := true;
     // AddLog(X.tostring + ',' + Y.tostring);
   end;
+end;
+
+{ TElem }
+
+constructor TElem.Create(AX, AY: int64; AHeatLoss: int64;
+ADirection: TDirection; ANbPasCourants: int64);
+begin
+  inherited Create;
+  X := AX;
+  Y := AY;
+  HeatLoss := AHeatLoss;
+  Direction := ADirection;
+  NbPasCourants := ANbPasCourants;
+end;
+
+{ TCellsVisited }
+
+function TCellsVisited.hasVisitedElem(AX, AY: int64; ADirection: TDirection;
+ANbPasCourant: int64): boolean;
+begin
+  Result := false;
+  for var Elem in self do
+    if (Elem.X = AX) and (Elem.Y = AY) and (Elem.Direction = ADirection) and
+      (Elem.NbPasCourants = ANbPasCourant) then
+    begin
+      Result := true;
+      break;
+    end;
+end;
+
+{ TMyPriorityQueue }
+
+function TMyPriorityQueue.Pop: TElem;
+begin
+  Result := self.Extractat(0);
+end;
+
+procedure TMyPriorityQueue.Push(Elem: TElem);
+begin
+  Add(Elem);
+  sort(TComparer<TElem>.construct(
+    function(const a, b: TElem): integer
+    begin
+      if a.HeatLoss < b.HeatLoss then
+        Result := -1
+      else if a.HeatLoss > b.HeatLoss then
+        Result := 1
+      else
+        Result := 0;
+    end));
 end;
 
 initialization
