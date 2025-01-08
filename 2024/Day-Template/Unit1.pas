1abe0f260498854f97f86a87a8df7389751ea684
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-01-08 16:47:25 +0100

    added Aoc 2024 template project

---
+++ 2024/Day-Template/Unit1.pas
@@ -0,0 +1,214 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.WinXCtrls,
+  Vcl.StdCtrls,
+  System.Generics.Collections;
+
+Const
+  // CDataFile = '..\..\input.txt';
+  CDataFile = '..\..\input-test.txt';
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    ActivityIndicator1: TActivityIndicator;
+    Button2: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    procedure BeginTraitement;
+    procedure EndTraitement;
+    { Déclarations privées }
+    function Exercice1: int64;
+    function Exercice2: int64;
+    procedure AddLog(Const S: String);
+    function MsToTimeString(ms: int64): string;
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  System.Math,
+  System.DateUtils,
+  System.RegularExpressions,
+  System.Generics.Defaults,
+  System.Diagnostics,
+  System.IOUtils;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  BeginTraitement;
+  Edit1.Text := 'Calcul en cours';
+  try
+    tthread.CreateAnonymousThread(
+      procedure
+      var
+        time: TStopwatch;
+      begin
+        try
+          try
+            time.Start;
+            try
+              Edit1.Text := Exercice1.tostring;
+            finally
+              time.Stop;
+              AddLog('Result : ' + Edit1.Text);
+              AddLog('Elapsed time : ' +
+                MsToTimeString(time.ElapsedMilliseconds));
+            end;
+            Edit1.SelectAll;
+            Edit1.CopyToClipboard;
+          finally
+            EndTraitement;
+          end;
+          ShowMessage(Edit1.Text + ' copié dans le presse papier.');
+        except
+          Edit1.Text := 'Erreur';
+        end;
+      end).Start;
+  except
+    EndTraitement;
+    Edit1.Text := 'Erreur';
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  BeginTraitement;
+  Edit2.Text := 'Calcul en cours';
+  try
+    tthread.CreateAnonymousThread(
+      procedure
+      var
+        time: TStopwatch;
+      begin
+        try
+          try
+            time.Start;
+            try
+              Edit2.Text := Exercice2.tostring;
+            finally
+              time.Stop;
+              AddLog('Result : ' + Edit2.Text);
+              AddLog('Elapsed time : ' +
+                MsToTimeString(time.ElapsedMilliseconds));
+            end;
+            Edit2.SelectAll;
+            Edit2.CopyToClipboard;
+          finally
+            EndTraitement;
+          end;
+          ShowMessage(Edit2.Text + ' copié dans le presse papier.');
+        except
+          Edit2.Text := 'Erreur';
+        end;
+      end).Start;
+  except
+    EndTraitement;
+    Edit2.Text := 'Erreur';
+  end;
+end;
+
+procedure TForm1.AddLog(const S: String);
+begin
+  tthread.Synchronize(nil,
+    procedure
+    begin
+      Memo1.Lines.Add(S);
+    end);
+end;
+
+procedure TForm1.BeginTraitement;
+begin
+  Button1.Enabled := false;
+  Button2.Enabled := false;
+  ActivityIndicator1.animate := true;
+end;
+
+procedure TForm1.EndTraitement;
+begin
+  tthread.Queue(nil,
+    procedure
+    begin
+      ActivityIndicator1.animate := false;
+      Button1.Enabled := true;
+      Button2.Enabled := true;
+    end);
+end;
+
+function TForm1.Exercice1: int64;
+var
+  Lig: integer;
+  Lignes: TArray<string>;
+begin
+  Lignes := tfile.ReadAllLines(CDataFile);
+  result := 0;
+  for Lig := 0 to length(Lignes) - 1 do
+  begin
+    // TODO : à compléter
+  end;
+end;
+
+function TForm1.Exercice2: int64;
+var
+  Lig: integer;
+  Lignes: TArray<string>;
+begin
+  Lignes := tfile.ReadAllLines(CDataFile);
+  result := 0;
+  for Lig := 0 to length(Lignes) - 1 do
+  begin
+    // TODO : à compléter
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Edit1.Text := '';
+  Edit2.Text := '';
+  Memo1.Clear;
+end;
+
+function TForm1.MsToTimeString(ms: int64): string;
+var
+  dt: TDatetime;
+  S: string;
+begin
+  dt := 0;
+  dt.addMilliSecond(ms);
+  S := dt.GetMilliSecond.tostring;
+  while length(S) < 3 do
+    S := '0' + S;
+  result := TimeToStr(dt) + ',' + S;
+end;
+
+initialization
+
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+{$ENDIF}
+
+end.
